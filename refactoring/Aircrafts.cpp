2e37655c9a0cdbe899cb50ac8056c41f7ebbeded
Bogdan <bogdan.plockiy@mail.ru>
2023-06-06 17:36:39 +0300

    перерисовал самолеты

--- refactoring/Aircrafts.cpp
+++ refactoring/Aircrafts.cpp
@@ -7,73 +7,85 @@
 
 void ABCAircraft::Wing()
 {
-	MoveToEx(m_hdc, GetShiftedX(50), GetShiftedY(75), NULL);
-	LineTo(m_hdc, GetShiftedX(65), GetShiftedY(0));
-	LineTo(m_hdc, GetShiftedX(60), GetShiftedY(100));
+	MoveToEx(m_hdc, GetShiftedX(40), GetShiftedY(50), NULL);
+	LineTo(m_hdc, GetShiftedX(35), GetShiftedY(100));
+	LineTo(m_hdc, GetShiftedX(45), GetShiftedY(100));
+	LineTo(m_hdc, GetShiftedX(60), GetShiftedY(50));
 };
 
 void ABCAircraft::Fuselage()
 {
-	MoveToEx(m_hdc, GetX(), GetY(), NULL);
+	MoveToEx(m_hdc, GetShiftedX(10), GetY(), NULL);
 	LineTo(m_hdc, GetShiftedX(20), GetY());
-	LineTo(m_hdc, GetShiftedX(25), GetShiftedY(40));
-	LineTo(m_hdc, GetShiftedX(90), GetShiftedY(40));
-	LineTo(m_hdc, GetShiftedX(100), GetShiftedY(70));
-	LineTo(m_hdc, GetShiftedX(90), GetShiftedY(100));
-	LineTo(m_hdc, GetShiftedX(5), GetShiftedY(100));
-	LineTo(m_hdc, GetX(), GetY());
+	LineTo(m_hdc, GetShiftedX(25), GetShiftedY(35));
+	LineTo(m_hdc, GetShiftedX(90), GetShiftedY(35));	// верхняя линия корпуса
+	LineTo(m_hdc, GetShiftedX(100), GetShiftedY(50));
+	LineTo(m_hdc, GetShiftedX(90), GetShiftedY(75));
+	LineTo(m_hdc, GetShiftedX(15), GetShiftedY(75));
+	LineTo(m_hdc, GetShiftedX(10), GetY());
 };
 
 void ABCAircraft::Window()
 {
-	MoveToEx(m_hdc, GetShiftedX(70), GetShiftedY(60), NULL);
-	LineTo(m_hdc, GetShiftedX(80), GetShiftedY(60));
-	LineTo(m_hdc, GetShiftedX(80), GetShiftedY(80));
-	LineTo(m_hdc, GetShiftedX(70), GetShiftedY(80));
-	LineTo(m_hdc, GetShiftedX(70), GetShiftedY(60));
+	MoveToEx(m_hdc, GetShiftedX(70), GetShiftedY(35), NULL);
+	LineTo(m_hdc, GetShiftedX(90), GetShiftedY(35));
+	LineTo(m_hdc, GetShiftedX(100), GetShiftedY(50));
+	LineTo(m_hdc, GetShiftedX(70), GetShiftedY(50));
+	LineTo(m_hdc, GetShiftedX(70), GetShiftedY(35));
 }
 
 void ABCAircraft::RearEngine()
 {
-	MoveToEx(m_hdc, GetX(), GetY(), NULL);
-	LineTo(m_hdc, GetShiftedX(10), GetShiftedY(30));
-	LineTo(m_hdc, GetShiftedX(0), GetShiftedY(40));
+	MoveToEx(m_hdc, GetShiftedX(10), GetShiftedY(40), NULL);
 	LineTo(m_hdc, GetShiftedX(0), GetShiftedY(50));
-	LineTo(m_hdc, GetShiftedX(10), GetShiftedY(60));
+	LineTo(m_hdc, GetShiftedX(0), GetShiftedY(60));
+	LineTo(m_hdc, GetShiftedX(15), GetShiftedY(70));
 }
 
 void ABCAircraft::UnderWingEngine()
 {
-	MoveToEx(m_hdc, GetX(), GetY(), NULL);
-	LineTo(m_hdc, GetShiftedX(50), GetShiftedY(40));
-	LineTo(m_hdc, GetShiftedX(60), GetShiftedY(50));
-	LineTo(m_hdc, GetShiftedX(60), GetShiftedY(60));
-	LineTo(m_hdc, GetShiftedX(50), GetShiftedY(70));
+	MoveToEx(m_hdc, GetShiftedX(55), GetShiftedY(65), NULL);
+	LineTo(m_hdc, GetShiftedX(65), GetShiftedY(75));
+	LineTo(m_hdc, GetShiftedX(65), GetShiftedY(85));
+	LineTo(m_hdc, GetShiftedX(50), GetShiftedY(90));
 }
 
 void ABCAircraft::BrokenFuselage()
 {
-	MoveToEx(m_hdc, GetX(), GetY(), NULL);
+
+	// фюзеляж
+	MoveToEx(m_hdc, GetShiftedX(10), GetY(), NULL);
 	LineTo(m_hdc, GetShiftedX(20), GetY());
-	LineTo(m_hdc, GetShiftedX(25), GetShiftedY(40));
-	LineTo(m_hdc, GetShiftedX(90), GetShiftedY(40));
-	LineTo(m_hdc, GetShiftedX(5), GetShiftedY(100));
-	LineTo(m_hdc, GetX(), GetY());
+	LineTo(m_hdc, GetShiftedX(25), GetShiftedY(35));
+	LineTo(m_hdc, GetShiftedX(40), GetShiftedY(35));
+
+	// разлом
+	LineTo(m_hdc, GetShiftedX(45), GetShiftedY(50));
+	LineTo(m_hdc, GetShiftedX(55), GetShiftedY(40));
+
+	// фюзеляж
+	LineTo(m_hdc, GetShiftedX(90), GetShiftedY(70));
+	LineTo(m_hdc, GetShiftedX(95), GetShiftedY(95));
+	LineTo(m_hdc, GetShiftedX(80), GetShiftedY(100));
+	LineTo(m_hdc, GetShiftedX(45), GetShiftedY(75));
+	LineTo(m_hdc, GetShiftedX(20), GetShiftedY(75));
+	LineTo(m_hdc, GetShiftedX(10), GetY());;
 }
 
 void ABCAircraft::BrokenWing()
 {
-	MoveToEx(m_hdc, GetX(), GetY(), NULL);
-	LineTo(m_hdc, GetShiftedX(65), GetShiftedY(0));
-	LineTo(m_hdc, GetShiftedX(60), GetShiftedY(100));
+	MoveToEx(m_hdc, GetShiftedX(40), GetShiftedY(50), NULL);
+	LineTo(m_hdc, GetShiftedX(42), GetShiftedY(70));
+	LineTo(m_hdc, GetShiftedX(20), GetShiftedY(100));
+	LineTo(m_hdc, GetShiftedX(30), GetShiftedY(100));
+	LineTo(m_hdc, GetShiftedX(60), GetShiftedY(50));
 }
 
 void ABCAircraft::BrokenWindow()
 {
-	MoveToEx(m_hdc, GetX(), GetY(), NULL);
-	LineTo(m_hdc, GetShiftedX(100), GetShiftedY(0));
-	LineTo(m_hdc, GetShiftedX(0), GetShiftedY(100));
-	LineTo(m_hdc, GetShiftedX(0), GetShiftedY(0));
+	MoveToEx(m_hdc, GetShiftedX(95), GetShiftedY(95), NULL);
+	LineTo(m_hdc, GetShiftedX(70), GetShiftedY(72));
+	LineTo(m_hdc, GetShiftedX(74), GetShiftedY(55));
 }
 
 ABCAircraft::ABCAircraft(HDC _hdc, HitBox _box, COLORREF _color, float _speed)
@@ -145,7 +157,7 @@ void LightAircraft::Draw()
 
 // Быстрый самолет
 FastAircraft::FastAircraft(HDC _hdc)
-	:ABCAircraft(_hdc, { 0,0,100,50 }, YELLOW, 10)
+	:ABCAircraft(_hdc, { 50,50,100,50 }, BLACK, 10)
 {}
 
 void FastAircraft::Draw()
@@ -158,7 +170,7 @@ void FastAircraft::Draw()
 
 // Очень быстрый самолет
 VeryFastAircraft::VeryFastAircraft(HDC _hdc)
-	:ABCAircraft(_hdc, { 0,0,100,50 }, GREEN, 15)
+	:ABCAircraft(_hdc, { 50,50,100,50 }, GREEN, 15)
 {}
 
 void VeryFastAircraft::Draw()
@@ -172,7 +184,7 @@ void VeryFastAircraft::Draw()
 
 // Сломанный самолет
 BrokenAircraft::BrokenAircraft(HDC _hdc)
-	:ABCAircraft(_hdc, { 0,0,100,50 }, BLACK, 0)
+	:ABCAircraft(_hdc, { 50,50,100,50 }, BLACK, 0)
 {}
 
 void BrokenAircraft::Draw()
